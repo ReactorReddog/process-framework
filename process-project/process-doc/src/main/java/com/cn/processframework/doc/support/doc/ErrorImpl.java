331f3a5479ebb164309e38afaadef420d84b46e4
chenxi19332@qq.com <123456.qq>
2021-08-24 17:26:05 +0800

    doc 大标题 小标题

--- process-project/process-doc/src/main/java/com/cn/processframework/doc/support/doc/ErrorImpl.java
+++ process-project/process-doc/src/main/java/com/cn/processframework/doc/support/doc/ErrorImpl.java
@@ -10,10 +10,25 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class ErrorImpl implements Error{
+    /**
+     * 错误码
+     */
     private String code;
+    /**
+     * 错误描述
+     */
     private String msg;
+    /**
+     * 错误详情码
+     */
     private String sub_code;
+    /**
+     * 错误详情描述
+     */
     private String sub_msg;
+    /**
+     * 解决方案
+     */
     private String solution;
 
 }
