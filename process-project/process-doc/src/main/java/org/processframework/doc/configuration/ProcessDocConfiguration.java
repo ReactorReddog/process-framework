2eff54d7534759e6dcdfd208df9ba6291fae5af6
chenxi19332@qq.com <123456.qq>
2021-08-31 18:01:22 +0800

    解决版本后为空的情况

--- process-project/process-doc/src/main/java/org/processframework/doc/configuration/ProcessDocConfiguration.java
+++ process-project/process-doc/src/main/java/org/processframework/doc/configuration/ProcessDocConfiguration.java
@@ -1,13 +1,23 @@
 package org.processframework.doc.configuration;
 
+import org.processframework.doc.DefaultCatelogConfigApi;
+import org.processframework.doc.api.CatelogConfigApi;
 import org.processframework.doc.properties.DocProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author apple
  * @desc doc配置
  * @since 1.0.0.RELEASE
  */
+@Configuration
 @EnableConfigurationProperties({DocProperties.class})
 public class ProcessDocConfiguration {
+
+    @ConditionalOnClass(CatelogConfigApi.class)
+    private CatelogConfigApi getCateLogConfigApi(){
+        return new DefaultCatelogConfigApi();
+    }
 }
