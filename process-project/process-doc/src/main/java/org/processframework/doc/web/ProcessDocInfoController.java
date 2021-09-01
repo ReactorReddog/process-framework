09ffd49e50b701d82e7938939e47401f53bdbe49
ASUS <chenxi19332@qq.com>
2021-09-01 17:07:48 +0800

    工具类及版本更新

--- process-project/process-doc/src/main/java/org/processframework/doc/web/ProcessDocInfoController.java
+++ process-project/process-doc/src/main/java/org/processframework/doc/web/ProcessDocInfoController.java
@@ -22,11 +22,26 @@ public class ProcessDocInfoController {
     public static final String DEFAULT_URL = "handler.document";
     @Autowired
     private CatelogConfigApi catelogConfigApi;
+    /**
+     * 获取开放平台目录
+     * @param scopeId 授权组
+     * @param treeId 树形id
+     * @param docId 文档id
+     * @param docType 文档类型
+     * @param tag 标签
+     * @return DocItemMenu
+     */
     @GetMapping("/catelogItemMenu")
     public DocItemMenu getApiCateLogItemMenu(Long scopeId,Long treeId,Long docId,Long docType,String tag){
         return catelogConfigApi.getApiCateLogItemMenu(scopeId,treeId,docId,docType,tag);
     }
-
+    /**
+     * 获取文档信息介绍
+     * @param treeId 树id 非必传
+     * @param docType 文档类型
+     * @param docId 文档id
+     * @return DocInfoDate
+     */
     @GetMapping("/document")
     public DocInfoDate getDocumentInfo(Long treeId, Long docType, Long docId, HttpServletRequest request){
         //获取请求语言
