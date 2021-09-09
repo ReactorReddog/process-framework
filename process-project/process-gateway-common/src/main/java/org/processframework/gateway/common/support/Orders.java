5cdd4548b2c913af345856c7481a26e9fb874e2f
ASUS <chenxi19332@qq.com>
2021-09-09 18:03:04 +0800

    优先级

---
+++ process-project/process-gateway-common/src/main/java/org/processframework/gateway/common/support/Orders.java
@@ -0,0 +1,27 @@
+package org.processframework.gateway.common.support;
+
+import org.springframework.core.Ordered;
+
+/**
+ * @author apple
+ * @desc 优先级别控制
+ * @since 1.0.0.RELEASE
+ */
+public class Orders {
+    /** 验证拦截器order */
+    public static final int VALIDATE_FILTER_ORDER = Ordered.HIGHEST_PRECEDENCE + 1000;
+
+    /** 参数格式化过滤器 */
+    public static final int PARAMETER_FORMATTER_FILTER_ORDER = VALIDATE_FILTER_ORDER + 1;
+
+    /** 权限验证过滤 */
+    public static final int PRE_ROUTE_PERMISSION_FILTER_ORDER = PARAMETER_FORMATTER_FILTER_ORDER + 100;
+
+    /** 验证拦截器order */
+    public static final int LIMIT_FILTER_ORDER = PRE_ROUTE_PERMISSION_FILTER_ORDER + 100;
+
+    /** 灰度发布过滤器 */
+    public static final int ENV_GRAY_FILTER_ORDER = LIMIT_FILTER_ORDER + 100;
+
+
+}
